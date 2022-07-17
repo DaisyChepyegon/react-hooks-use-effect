8534cb5d775a184cf45fe6d8f0d4620ae607e784
DaisyChepyegon <daisychepyegon@gmail.com>
2022-07-17 13:50:45 +0300

    Done.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
 import React, { useState, useEffect } from "react";
 
 function App() {
+const [count, setCount] = useState(0);
+const [text, setText] = useState("");
+
   useEffect(() => {
-    console.log("useEffect called");
-  });
+    setTimeout(() => setCount(0),5000)
+
+    
+  } ,[]);
 
   console.log("Component rendering");
 
-  return <button>Click Me</button>;
+  return (
+    <div>
+  <button onClick={() => setCount((count) => count + 1)}>
+    {count} times
+  </button>
+  <input
+    type="text"
+    placeholder="type here"
+    value={text}
+    onChange={(e) => setText(e.target.value)}
+    />
+
+  </div>
+  )
+
 }
 
 export default App;
